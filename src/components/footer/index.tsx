d65e22c86b3f38d79d0f1e141d160b64b018340e
quan.ta <quan.ta@geekup.vn>
2023-01-06 12:18:42 +0700

    fix header and footer -> more responsive

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,8 +4,9 @@ import BKlogo from "../../assets/images/hcmut.png";
 const Footer: React.FC = (props: any) => {
     return (
         <div className='w-full relative bottom-0'>
-            <div className='bg-footer flex'>
-                <div className='pl-[70px]'>
+            <div className='bg-footer flex justify-center py-5'>
+                <div className='flex px-10'>
+                <div className='pl-[0px]'>
                     <div className='flex justify-center items-center'>
                         <img 
                             src={BKlogo} 
@@ -13,50 +14,50 @@ const Footer: React.FC = (props: any) => {
                             className='h-20 w-20'
                         />
                     </div>
-                    <div className='text-white font-bold flex justify-center items-center'>
-                    ĐẠI HỌC BÁCH
-                    </div>
-                    <div className='text-white font-bold flex justify-center items-center'>
-                    KHOA TPHCM
+                    <div className='text-white font-bold flex justify-center items-center text-sm'>
+                    ĐẠI HỌC BÁCH KHOA TPHCM
                     </div>
                 </div>
 
-                <div className='pl-[450px]'>
+                <div className='pl-[400px]'>
                     <div className='text-white font-bold flex justify-center items-center py-3'>
                         WEBSITE
                     </div>
                     <div>
-                        <div className='text-white font-medium'>
+                        <div className='text-white font-medium text-xs'>
                             HCMUT
                         </div>
-                        <div className='text-white font-medium'>
+                        <div className='text-white font-medium text-xs'>
                             MyBK
                         </div>
-                        <div className='text-white font-medium'>
+                        <div className='text-white font-medium text-xs'>
                             BKeL
                         </div>
                     </div>
                 </div>
 
-                <div className='pl-[550px]'>
+                <div className='pl-[400px]'>
                     <div className='text-white font-bold flex items-center py-3'>
                         CONTACT
                     </div>
                     <div>
-                        <div className='text-white font-medium'>
+                        <div className='text-white font-medium text-xs'>
                             268 Ly Thuong Kiet Street Ward 14, District 10, Ho Chi Minh City, Vietnam
                         </div>
-                        <div className='text-white font-medium'>
+                        <div className='text-white font-medium text-xs'>
                             (028) 38 651 670 - (028) 38 647 256 (Ext: 5258, 5234)
                         </div>
-                        <div className='text-white font-medium'>
+                        <div className='text-white font-medium text-xs'>
                             elearning@hcmut.edu.vn
                         </div>
                     </div>
                 </div>
+                </div>
+
             </div>
+
             <div className='bg-[#002ECF] flex justify-center items-center'>
-                <div className='text-white font-bold p-2'>
+                <div className='text-white font-bold p-1 text-sm'>
                     COPYRIGHT 2022-2023
                 </div>
             </div>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -19,10 +19,10 @@ const Header: React.FC<Props> = (props: any) => {
     const {isLogin, isAccountServicePage} = props;
 
     return (
-        <div className='bg-white grid grid-cols-10 gap-4 p-3 mb-1 max-h-17 border-2 sticky top-0'>
+        <div className='bg-white grid grid-cols-12 gap-4 p-3 mb-1 max-h-17 border-2 sticky top-0'>
             {/* The BK icon and the HCMUT name */}
             <Link
-                className='col-start-1 col-span-3 flex items-center hover:cursor-pointer'
+                className='col-start-1 col-span-5 flex items-center hover:cursor-pointer'
                 to={`/${appRouters.LINK_TO_HOME_PAGE}`}
             >
                 <img
@@ -45,14 +45,14 @@ const Header: React.FC<Props> = (props: any) => {
                 * Only appear when user not logged in and the page is not an account service page
             */}
             {!isLogin && !isAccountServicePage && (
-            <div className='col-start-9 col-span-2 flex items-center'>
+            <div className='col-start-11 col-span-5 flex items-center'>
                 <Link to={`/${appRouters.LINK_TO_REGISTER_PAGE}`}>
-                    <div className="text-[#0079CC] font-semibold py-2 px-5 no-underline hover:underline hover:cursor-pointer">
+                    <div className="text-[#0079CC] text-xs font-semibold py-2 px-5 no-underline hover:underline hover:cursor-pointer">
                     Đăng ký tài khoản?
                     </div>
                 </Link>
                 <Link to={`/${appRouters.LINK_TO_LOGIN_PAGE}`}>
-                    <div className="bg-[#0079CC] transition text-white font-semibold py-4 px-5 border border-white-500 rounded-[15px] hover:bg-[#025A97] hover:cursor-pointer">
+                    <div className="bg-[#0079CC] text-xs transition text-white font-semibold py-4 px-5 border border-white-500 rounded-[15px] hover:bg-[#025A97] hover:cursor-pointer">
                     ĐĂNG NHẬP
                     </div>
                 </Link>
@@ -60,19 +60,20 @@ const Header: React.FC<Props> = (props: any) => {
 
             {/* Avatar and drop down menu */}
             {isLogin && !isAccountServicePage && (
-            <div className='col-start-10 col-span-2'>
+            <div className='col-start-12 col-span-5'>
                 <div 
                 className='relative flex items-center hover:cursor-pointer pt-5'
                 onClick = {() => setIsDroppedDown(!isDroppedDown)}
                 >
                     <img
-                    className="p-1 w-9 h-9 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 inline-block mr-2"
+                    className="p-1 w-9 h-9 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 inline-block mr-5"
                     src="https://media.istockphoto.com/vectors/three-persons-icon-black-vector-vector-id1158561473?k=20&m=1158561473&s=612x612&w=0&h=pSRNS3mkeYMYcleK_Pzf89gnkVQuxtiSGMm4yll-UXg="
                     alt="Bordered avatar"
                     ></img>
 
                     <div
-                    style={{ fontSize: "16px", fontWeight: "bold", width: "150px" }}
+                    // style={{ fontSize: "16px", fontWeight: "bold", width: "150px" }}
+                    className='text-sm font-bold'
                     >
                         Trần Anh Quân
                     </div>
@@ -105,8 +106,6 @@ const Header: React.FC<Props> = (props: any) => {
                     </div>
                 </div>
             </div>)}
-
-
         </div>
     );
 }
