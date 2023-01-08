2c8cb2e1a69faed2aac9b3a067d7005faa0c7850
quan.ta <quan.ta@geekup.vn>
2023-01-08 13:53:27 +0700

    add active status to sideNav items + fix responsive for sideNav + remove footer from dashboard + add role type

--- src/pages/faculty/secretary/expenseStatisticsPage/index.tsx
+++ src/pages/faculty/secretary/expenseStatisticsPage/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import Footer from '../../../../components/footer';
 import Header from '../../../../components/header';
 import SideNav from '../../../../components/sideNav';
+import { RoleType } from '../../../../shared/types/role';
 
 const FSExpenseStatistics: React.FC = () => {
     return (
         <div className=''>
             <Header isLogin={true} isAccountServicePage={false}/>
-            <SideNav role={"fs"}/>
-            <Footer />
+            <SideNav role={RoleType.FS} pathName={"/"}/>
         </div>
     )
 }
--- src/pages/faculty/viceDean/expenseStatisticsPage/index.tsx
+++ src/pages/faculty/viceDean/expenseStatisticsPage/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import Footer from '../../../../components/footer';
 import Header from '../../../../components/header';
 import SideNav from '../../../../components/sideNav';
+import { RoleType } from '../../../../shared/types/role';
 
 const FVDExpenseStatistics: React.FC = () => {
     return (
         <div className=''>
             <Header isLogin={true} isAccountServicePage={false}/>
-            <SideNav role={"fvd"}/>
-            <Footer />
+            <SideNav role={RoleType.FVD} pathName={'/'}/>
         </div>
     )
 }
