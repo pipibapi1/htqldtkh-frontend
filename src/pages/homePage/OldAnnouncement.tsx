33d950d9ecfc86077b10e454fa2ecd61ce04b8bd
quan.ta <quan.ta@geekup.vn>
2023-02-19 21:04:50 +0700

    update upload announcement

--- src/pages/homePage/OldAnnouncement.tsx
+++ src/pages/homePage/OldAnnouncement.tsx
@@ -14,7 +14,7 @@ const OldAnnouncement = ({oldAnnouncements}: {oldAnnouncements: AnnouncementType
     return (
       <div className = "col-span-1">
         <div className = 'px-4 py-6 mx-4 justify-between items-center'>
-          <div className = 'py-4 text-blue-600 font-semibold'>MỘT SỐ THÔNG BÁO ĐỢT TRƯỚC</div>
+          <div className = 'py-4 text-blue-600 font-semibold'>CÁC THÔNG BÁO CŨ</div>
 
           <div className = 'grid gap-10 mb-12'> 
             {oldAnnouncements.map((SmallAnnouncement, index) => 
