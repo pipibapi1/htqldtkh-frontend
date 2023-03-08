7b5c68ef890f22c847756ac8da3a5de91e7a9b93
quan.ta <quan.ta@geekup.vn>
2023-03-08 20:15:17 +0700

    fix create council modal

--- src/pages/faculty/secretary/createCouncilPage/Council1/CouncilStepController/Step1.tsx
+++ src/pages/faculty/secretary/createCouncilPage/Council1/CouncilStepController/Step1.tsx
@@ -153,7 +153,7 @@ export default function Step1() {
                         </div>
                         <input
                             className = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder = "Ngày diễn ra"
+                            placeholder="dd-mm-yyyy"
                             value={council.date}
                             onChange={onChangeCouncilDate}
                             type="date"
--- src/pages/faculty/secretary/createCouncilPage/Council2/AddCouncilModal.tsx
+++ src/pages/faculty/secretary/createCouncilPage/Council2/AddCouncilModal.tsx
@@ -47,12 +47,12 @@ const AddCouncilModal = ({isVisible, onClose}: {isVisible: boolean, onClose: any
     };
 
     return (
-        <div className = "fixed inset-0 bg-black bg-opacity-50 backdrop-blur-0 flex justify-center items-center" id= "wrapper" onClick={handleClose}>
-            <div className = "md:w-[600px] w-[90%] mx-auto">
+        <div className = "fixed inset-0 bg-black bg-opacity-50 backdrop-blur-0 flex justify-center items-center z-40" id= "wrapper" onClick={handleClose}>
+            <div className = "w-[60%] h-[92%] mx-auto">
                 <div className = 'bg-white rounded p-2'>
                     <div className = "py-6 px-6 lg:px-8 text-left">
                         <div className = 'mb-8 pb-4 text-xl font-medium text-gray-900 text-center border-b-2 border-black'>
-                            Thêm hội đồng
+                            Thêm hội đồng nghiệm thu
                         </div>
                     </div>
 
